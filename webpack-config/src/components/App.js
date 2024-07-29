dcff5ab62b2be4db9725618869228700068f4a76
Ngoc Khanh <khanhk56a12@gmail.com>
2024-07-29 23:26:47 +0700

    Setup react babel

---
+++ webpack-config/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            This is my react app
+        </div>
+    )
+}
+
+export default App;
---
+++ webpack-config/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
